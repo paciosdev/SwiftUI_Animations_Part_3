7ac7e87f1d7ecec1a18cf8aabe053b9e10ac4f3e
Francesco Paciello <kekkopacio@gmail.com>
2021-03-29 17:50:42 +0200

    Initial Commit

---
+++ Hue Rotation Example 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hue Rotation Example 1
+//
+//  Created by Kekko Paciello on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hue Rotation Example 1/Hue_Rotation_Example_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_Rotation_Example_1App.swift
+//  Hue Rotation Example 1
+//
+//  Created by Kekko Paciello on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Hue_Rotation_Example_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
